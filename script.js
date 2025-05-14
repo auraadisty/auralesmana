a82acfc33e67d9289375ab3953ef21414cf2baf2
auraadisty <auraadisty2@gmail.com>
2025-05-14 21:45:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,15 @@ document.addEventListener('DOMContentLoaded', function () {
 function toggleDropdown() {
   document.querySelector('.dropdown-menu').classList.toggle('show');
 }
+
+ const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
