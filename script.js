d24717dce705ad2574b4bab55eae9a4d8510a7de
auraadisty <auraadisty2@gmail.com>
2025-05-16 10:54:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,42 @@ function toggleDropdown() {
       prevEl: '.swiper-button-prev',
     },
   });
+
+// Fungsi untuk menyetel cookie
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + (days*24*60*60*1000));
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+// Fungsi untuk membaca cookie
+function getCookie(name) {
+  const cname = name + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1);
+    if (c.indexOf(cname) == 0) return c.substring(cname.length, c.length);
+  }
+  return "";
+}
+
+// Fungsi menyimpan nama pengguna ke cookie
+function setUserName() {
+  const name = document.getElementById("username").value;
+  setCookie("userName", name, 7);
+  showWelcome();
+}
+
+// Fungsi menampilkan pesan sambutan
+function showWelcome() {
+  const user = getCookie("userName");
+  if (user) {
+    document.getElementById("welcomeMessage").innerText = "Selamat datang kembali, " + user + "!";
+  }
+}
+
+// Panggil saat halaman dimuat
+document.addEventListener("DOMContentLoaded", showWelcome);
